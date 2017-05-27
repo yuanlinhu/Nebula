4754504bba07232df2dcb54fe05209e024418b70
yuanlinhu <linhu917@126.com>
2017-05-27 17:00:31 +0800

    ddd

--- Source/server/gate_server/main.cpp
+++ Source/server/gate_server/main.cpp
@@ -2,12 +2,17 @@
 
 #include <event2/event.h>
 
+#include "player.h"
+
 using namespace std;
 
 
 
 int main()
 {
+    Player player;
+	int result = player.add(3, 5);
+    cout<<"result = "<<result<<endl;
     event_base* base = event_base_new();
 
     return 0;
