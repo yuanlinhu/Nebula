28c4c91d31b306f7feeb908d1f3b1853d7b11f05
yuanlinhu <linhu917@126.com>
2017-12-30 13:55:00 +0800

    game_server/main.cpp

--- Source/server/game_server/main.cpp
+++ Source/server/game_server/main.cpp
@@ -7,24 +7,81 @@
 using namespace std;  
 using namespace tutorial;  
 
+static string file_name = "./1.file";
+void write_proto()
+{
+ 	Person person;  
+ 
+ 	person.set_name("flamingo32345 eewwsd ");     
+ 	person.set_id(1234565);     
+ 
+ 	cout<<person.name()<<endl;  
+	cout<<person.id()<<endl;  
+
+
+ 	fstream output(file_name.c_str(), ios::out | ios::trunc | ios::binary);
+ 	if (!person.SerializeToOstream(&output)) {
+ 		cerr << "Failed to write address book."<<endl;
+ 	}
+}
+
+
+void read_proto()
+{
+	Person person_read;
+
+	{
+		// Read the existing address book.
+		fstream input(file_name.c_str(), ios::in | ios::binary);
+		if (!person_read.ParseFromIstream(&input)) {
+			cerr << "Failed to parse address book." << endl;
+			return ;
+		}
+	}
+
+	cout<<person_read.name()<<endl;  
+	cout<<person_read.id()<<endl;  
+}
+
 int main()  
 {  
-	Person person;  
 
-	person.set_name("flamingo");     
-	person.set_id(18);     
+	write_proto();
+	read_proto();
 
-	cout<<person.name()<<endl;  
+// 	Person person;  
+// 
+// 	person.set_name("flamingo32345");     
+// 	person.set_id(18);     
+// 
+// 	cout<<person.name()<<endl;  
 	//cout<<person.age()<<endl;  
 
 
-	fstream output(argv[1], ios::out | ios::trunc | ios::binary);
-	if (!person.SerializeToOstream(&output)) {
-		cerr << "Failed to write address book."<<endl;
+// 	fstream output("./1.file", ios::out | ios::trunc | ios::binary);
+// 	if (!person.SerializeToOstream(&output)) {
+// 		cerr << "Failed to write address book."<<endl;
+// 	}
+
+	//Person person1;
+	
+
+	tutorial::Person person_read;
+
+	{
+		// Read the existing address book.
+		fstream input("./1.file", ios::in | ios::binary);
+		if (!person_read.ParseFromIstream(&input)) {
+			cerr << "Failed to parse address book." << endl;
+			return -1;
+		}
 	}
 
-	Person person1;
-	
+	cout<<person_read.name()<<endl;  
+	cout<<person_read.id()<<endl;  
+
+
+
 
 
 	system("pause");  
